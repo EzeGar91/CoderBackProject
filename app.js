c3eee0a0a413e5cdada21b84850f5cd91c7bb968
Ezequiel Garriga <ezegarriga.cba@gmail.com>
2022-12-01 01:38:58 -0300

    Sincronizando App.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require("express"),
   app = express(),
-  bodyParser = require("body-parser"),
-  methodOverride = require("method-override");
-mongoose = require("mongoose");
+  http = require("http"),
+  server = http.createServer(app),
+  mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
 var router = express.Router();
 
+//Rutas
 router.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Bienvenido a la tienda de peliculas");
 });
 
 app.use(router);
 
-app.listen(3000, function () {
-  console.log("Servidor corriendo en http://localhost:3000");
+// Conexion MongoDB
+mongoose.connect("mongodb+srv://EzeGarriga:Arg911809!@ezegcluster.dftxr2r.mongodb.net/?retryWrites=true&w=majority", function (err, res) {
+  if (err) {
+    console.log("ERROR: connecting to Database. " + err);
+  }
+  app.listen(27017, function () {
+    console.log("Servidor corriendo en http://localhost:27017");
+  });
 });
+
